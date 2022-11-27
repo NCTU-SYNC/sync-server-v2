530e501e03f0a704b424f1d878d901333d24bcc6
David Chiu <david20571015@gmail.com>
2022-11-28 04:58:18 +0800

    feat: github action for build openapi doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  SwaggerModule,
+  SwaggerCustomOptions,
+  DocumentBuilder,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('SYNC API')
+    .setDescription('The SYNC API spec')
+    .setVersion('2.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  const customOptions: SwaggerCustomOptions = {
+    explorer: true,
+  };
+
+  SwaggerModule.setup('api', app, document, customOptions);
+
   await app.listen(3000);
 }
 bootstrap();
