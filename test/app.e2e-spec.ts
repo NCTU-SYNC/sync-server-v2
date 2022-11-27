d0abdd796cd3568e86e657a24d89f4ce78d394e6
David Chiu <david20571015@gmail.com>
2022-11-28 05:52:39 +0800

    CI/Iopenapi doc (#10)
    * feat: use test to generate api doc
    
    * fix: script to generate swaggwer ui
    
    * fix: file name of swagger ui target

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './../src/app.module';
+import * as path from 'path';
+import * as fs from 'fs';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -12,5 +15,21 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should generate swagger spec', async () => {
+    const config = new DocumentBuilder()
+      .setTitle('SYNC API')
+      .setDescription('The SYNC API spec')
+      .setVersion('2.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    const outputPath = path.resolve(process.cwd(), 'swagger.json');
+    fs.writeFileSync(outputPath, JSON.stringify(document));
   });
 });
