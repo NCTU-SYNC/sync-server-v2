73b085431b086cca35b43f0a3b473a2fb70d415b
David Chiu <david20571015@gmail.com>
2023-01-27 23:38:10 +0800

    fix: set default port

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 const ENV_CONFIG = () => ({
-  port: parseInt(process.env.PORT, 10) | 3000,
+  port: parseInt(process.env.PORT, 10) || 3000,
   baseUrl: '/api',
 });
 
