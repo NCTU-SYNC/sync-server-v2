cb6b42b69ebb1e16a6a5d0c58c239b3a89f9ec4b
David Chiu <david20571015@gmail.com>
2023-03-20 13:35:52 +0800

    refactor: use `Module` to initial firebase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { FirebaseModule } from './auth/firebase.module';
 
 const ENV_CONFIG = () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
@@ -12,6 +13,7 @@ const ENV_CONFIG = () => ({
       load: [ENV_CONFIG],
       envFilePath: ['config/firebase.env'],
     }),
+    FirebaseModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/auth/firebase.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { FirebaseService } from './firebase.service';
+
+@Module({
+  imports: [ConfigModule],
+  controllers: [],
+  providers: [FirebaseService],
+  exports: [FirebaseService],
+})
+export class FirebaseModule {}
