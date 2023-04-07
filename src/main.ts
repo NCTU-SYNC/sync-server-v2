34d80e12483496b5ff7142013289175edd956884
David Chiu <david20571015@gmail.com>
2023-04-07 23:28:38 +0800

    Revert "Swagger (#15)"
    This reverts commit 680ef850a4083f83cc37d1211b625944dfa914a2.

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,8 @@ import {
   DocumentBuilder,
 } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as path from 'path';
-import * as fs from 'fs';
 
-function createSwaggerDocument(app: INestApplication) {
+function setupSwagger(app: INestApplication) {
   const config = new DocumentBuilder()
     .setTitle('SYNC API')
     .setDescription('The SYNC API spec')
@@ -19,34 +17,25 @@ function createSwaggerDocument(app: INestApplication) {
 
   const document = SwaggerModule.createDocument(app, config);
 
-  return document;
+  const customOptions: SwaggerCustomOptions = {
+    explorer: true,
+  };
+
+  SwaggerModule.setup('doc', app, document, customOptions);
 }
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const document = createSwaggerDocument(app);
+  setupSwagger(app);
 
   const configService = app.get(ConfigService);
+  const port = configService.get<number>('port', 3000);
+  const baseUrl = configService.get<string>('baseUrl', '/api');
 
-  if (configService.get('NODE_ENV') === 'document') {
-    const outputPath = path.resolve(process.cwd(), 'swagger.json');
-    fs.writeFileSync(outputPath, JSON.stringify(document));
-
-    await app.close();
-  } else {
-    const customOptions: SwaggerCustomOptions = {
-      explorer: true,
-    };
-    SwaggerModule.setup('doc', app, document, customOptions);
-
-    const port = configService.get<number>('port', 3000);
-    const baseUrl = configService.get<string>('baseUrl', '/api');
-
-    app.setGlobalPrefix(baseUrl);
-    app.enableVersioning({ type: VersioningType.URI });
-    await app.listen(port);
-  }
+  app.setGlobalPrefix(baseUrl);
+  app.enableVersioning({ type: VersioningType.URI });
+  await app.listen(port);
 }
 
 bootstrap();
