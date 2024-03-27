3d1884a8cc8b3146afc3fe942cd4068a61e28341
richartothman <richartothman@gmail.com>
2024-03-27 21:49:21 +0800

    Added Controller for getArticle and getArticlebyID

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { FirebaseModule } from './auth/firebase.module';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { ArticleController } from './database/controllers/article.controller';
+import { ArticleService } from './database/article.service'; 
+import { Article, ArticleSchema } from './database/schemas/article.schema'; 
 const ENV_CONFIG = () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   baseUrl: '/api',
@@ -30,8 +32,9 @@ const ENV_CONFIG = () => ({
         uri: config.get<string>('MONGODB_URI'),
       }),
     }),
+    MongooseModule.forFeature([{ name: Article.name, schema: ArticleSchema }]),
   ],
-  controllers: [],
-  providers: [],
+  controllers: [ArticleController],
+  providers: [ArticleService],
 })
 export class AppModule {}
