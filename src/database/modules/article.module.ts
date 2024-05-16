c4475c4af5cb82dae90a4abb3e83fa1708e12aa6
David Chiu <david20571015@gmail.com>
2024-05-16 23:26:39 +0800

    fix: lint

--- src/database/modules/article.module.ts
+++ src/database/modules/article.module.ts
@@ -4,12 +4,11 @@ import { ArticleService } from '../article.service';
 import { Article, ArticleSchema } from '../schemas/article.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 
-
 @Module({
-    imports: [
-        MongooseModule.forFeature([{ name: Article.name, schema: ArticleSchema }])
-    ],
-    controllers: [ArticleController],
-    providers: [ArticleService],
+  imports: [
+    MongooseModule.forFeature([{ name: Article.name, schema: ArticleSchema }]),
+  ],
+  controllers: [ArticleController],
+  providers: [ArticleService],
 })
 export class ArticleModule {}
